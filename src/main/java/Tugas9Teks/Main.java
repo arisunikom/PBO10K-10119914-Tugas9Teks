9cff21835f6da0744e81a49e3af5edfa17e0ff2d
dropsuite <dropsuite@172.20.10.2>
2020-10-20 19:46:46 +0700

    Initial Commit

---
+++ src/main/java/Tugas9Teks/Main.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tugas9Teks;
+
+/**
+ *
+ * @author Aris Prabowo
+ * IF10k
+ * 10119914
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        char c;
+        char tab = '\t';
+        char newLine = '\n';
+        c = 'A';
+        System.out.println("Hasil" + newLine + "Char: " + tab + "berisi " + c);
+    }
+    
+}
